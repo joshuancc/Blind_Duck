cc89744e45a4daee5e6dfd026b4b20a0f9a7045c
Calvin Tran <calvintran1478@protonmail.com>
2024-08-20 17:53:42 -0400

    Add CORS handling to server

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 import apiV1Router from "./routes/v1/api-routes.js";
 
 // Set up server
 const PORT = 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use("/api/v1", apiV1Router);
 
