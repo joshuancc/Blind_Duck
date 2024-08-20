353d30cd5a30327b893d100093eb4fe936ba50c9
Joshua Ng <joshuanccng@gmail.com>
2024-08-20 15:43:23 -0400

    finished token verify

--- server/src/routes/v1/customer-routes.js
+++ server/src/routes/v1/customer-routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import validateRequest from "../../middleware/validation-middleware.js";
+import auth from "../../middleware/auth.js";
 import { registerCustomerSchema, loginCustomerSchema } from "../../schemas/v1/customer-schemas.js";
-import { registerCustomer,loginCustomer } from "../../controllers/v1/customer-controller.js";
+import { registerCustomer,loginCustomer,addItem } from "../../controllers/v1/customer-controller.js";
 
 
 const customerRouter = express.Router();
 
 customerRouter.post("/", [validateRequest(registerCustomerSchema)], registerCustomer);
 customerRouter.post("/login",[validateRequest(loginCustomerSchema)], loginCustomer);
+customerRouter.post('/addItem',[validateRequest(loginCustomerSchema),auth], addItem); 
 export default customerRouter;
