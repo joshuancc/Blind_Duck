fa0f56d25b0e46c0658ca5f316c7b3a51baae07d
Joshua Ng <joshuanccng@gmail.com>
2024-08-20 21:24:15 -0400

    created MenuItemTable

--- server/src/models/admin-model.js
+++ server/src/models/admin-model.js
@@ -7,6 +7,27 @@ const adminSchema = mongoose.Schema({
     password: String
 })
 
+const MenuItemTable = mongoose.Schema ({
+    name: { type: String,
+        required : true,
+        unqique: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      validate : {
+        validator : !Number.isInteger
+    },
+    min: 0},
+
+    storage:{
+      type: Number,
+      required: true,
+      validate : {
+        validator : Number.isInteger
+    }, min: 0}
+})
+
 const Admin = mongoose.model("Admin", adminSchema);
 
 export default Admin;
--- server/src/models/customer-model.js
+++ server/src/models/customer-model.js
@@ -5,6 +5,28 @@ const customerSchema = mongoose.Schema({
     username: String,
     password: String
 })
+const MenuItemTable = mongoose.Schema ({
+    name: { type: String,
+        required : true,
+        unqique: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      validate : {
+        validator : !Number.isInteger
+    },
+    min: 0},
+
+    storage:{
+      type: Number,
+      required: true,
+      validate : {
+        validator : Number.isInteger
+    }, min: 0}
+})
+
+
 
 const Customer = mongoose.model("Customer", customerSchema);
 
